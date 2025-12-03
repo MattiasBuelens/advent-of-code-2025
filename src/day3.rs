9549ea7917738c58e01ec1903b533a7b654b5aff
Mattias Buelens <mattias@buelens.com>
2025-12-03 09:26:48 +0100

    Start solving day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,20 +1,20 @@
 use aoc_runner_derive::{aoc, aoc_generator};
+use std::cmp::max;
 
 type Bank = Vec<u8>;
 
-#[aoc_generator(day3)]
-fn parse(input: &str) -> Vec<Bank> {
-    input
-        .lines()
-        .map(|line| {
-            line.chars()
-                .map(|c| c.to_digit(10).unwrap() as u8)
-                .collect()
-        })
+fn parse_bank(line: &str) -> Bank {
+    line.chars()
+        .map(|c| c.to_digit(10).unwrap() as u8)
         .collect()
 }
 
-fn max_joltage(bank: &[u8]) -> u8 {
+#[aoc_generator(day3)]
+fn parse(input: &str) -> Vec<Bank> {
+    input.lines().map(parse_bank).collect()
+}
+
+fn max_joltage_part1(bank: &[u8]) -> u8 {
     bank.iter()
         .copied()
         .enumerate()
@@ -28,12 +28,39 @@ fn max_joltage(bank: &[u8]) -> u8 {
 
 #[aoc(day3, part1)]
 fn part1(input: &[Bank]) -> u64 {
-    input.iter().map(|bank| max_joltage(bank) as u64).sum()
+    input
+        .iter()
+        .map(|bank| max_joltage_part1(bank) as u64)
+        .sum()
+}
+
+fn max_joltage_part2(bank: &[u8], len: usize) -> u64 {
+    debug_assert!(len > 2);
+    let mut max_joltages: Vec<Vec<u64>> = Vec::with_capacity(len + 1);
+    // Ignore len = 0
+    max_joltages.push(Vec::new());
+    // With len = 1, the maximum joltage is just the battery's value
+    max_joltages.push(bank.iter().map(|&x| x as u64).collect());
+    let mut shift = 10;
+    for len in 2..=len {
+        max_joltages.push(vec![0u64; bank.len()]);
+        for (i, &battery) in bank.iter().enumerate().rev().skip(len - 1) {
+            // Two options:
+            // - Take this battery and add the max joltage at i+1 of the previous length
+            // - Skip this battery and use the max joltage at i+1 of the current length
+            max_joltages[len][i] = max(
+                max_joltages[len - 1][i + 1] + (battery as u64 * shift),
+                max_joltages[len][i + 1],
+            );
+        }
+        shift *= 10;
+    }
+    max_joltages[len].iter().copied().max().unwrap()
 }
 
 #[aoc(day3, part2)]
 fn part2(input: &[Bank]) -> u64 {
-    todo!()
+    input.iter().map(|bank| max_joltage_part2(bank, 12)).sum()
 }
 
 #[cfg(test)]
@@ -52,6 +79,26 @@ mod tests {
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse(EXAMPLE)), 0);
+        assert_eq!(part2(&parse(EXAMPLE)), 3121910778619);
+    }
+
+    #[test]
+    fn test_max_joltage_part2() {
+        assert_eq!(
+            max_joltage_part2(&parse_bank("987654321111111"), 12),
+            987654321111
+        );
+        assert_eq!(
+            max_joltage_part2(&parse_bank("811111111111119"), 12),
+            811111111119
+        );
+        assert_eq!(
+            max_joltage_part2(&parse_bank("234234234234278"), 12),
+            434234234278
+        );
+        assert_eq!(
+            max_joltage_part2(&parse_bank("818181911112111"), 12),
+            888911112111
+        );
     }
 }
