2828d571cfd217c49872db433ef5df5e3eb226a7
Mattias Buelens <mattias@buelens.com>
2025-12-04 08:38:04 +0100

    Solve day 4 part 2

--- src/day4.rs
+++ src/day4.rs
@@ -19,22 +19,39 @@ fn parse(input: &str) -> Grid {
         .collect()
 }
 
-#[aoc(day4, part1)]
-fn part1(grid: &Grid) -> usize {
+fn removable_rolls(grid: &Grid) -> Vec<Vector2D> {
     grid.iter()
-        .filter(|&&roll| {
+        .copied()
+        .filter(|&roll| {
             let neighbour_rolls = roll
                 .neighbours_diagonal()
                 .filter(|pos| grid.contains(pos))
                 .count();
             neighbour_rolls < 4
         })
-        .count()
+        .collect()
+}
+
+#[aoc(day4, part1)]
+fn part1(grid: &Grid) -> usize {
+    removable_rolls(grid).len()
 }
 
 #[aoc(day4, part2)]
 fn part2(grid: &Grid) -> usize {
-    todo!()
+    let mut grid = grid.clone();
+    let mut total_removed = 0usize;
+    loop {
+        let removable = removable_rolls(&grid);
+        if removable.is_empty() {
+            break;
+        }
+        total_removed += removable.len();
+        for roll in removable {
+            grid.remove(&roll);
+        }
+    }
+    total_removed
 }
 
 #[cfg(test)]
@@ -59,6 +76,6 @@ mod tests {
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse(EXAMPLE)), 0);
+        assert_eq!(part2(&parse(EXAMPLE)), 43);
     }
 }
