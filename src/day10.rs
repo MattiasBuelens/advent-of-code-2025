eb6aa305e06a2e04e0bb894fcf263f70108b1d48
Mattias Buelens <mattias@buelens.com>
2025-12-19 17:52:11 +0100

    Solve day 10 part 1

--- src/day10.rs
+++ src/day10.rs
@@ -55,9 +55,49 @@ fn parse(input: &str) -> Vec<Machine> {
         .collect()
 }
 
+fn configure(machine: &Machine) -> BitVec {
+    solve_recurse(
+        machine,
+        &bitvec!(0; machine.lights.len()),
+        BitVec::with_capacity(machine.buttons.len()),
+    )
+    .expect("no solution")
+}
+
+fn solve_recurse(machine: &Machine, lights: &BitVec, presses: BitVec) -> Option<BitVec> {
+    if presses.len() == machine.buttons.len() {
+        return if &machine.lights == lights {
+            Some(presses)
+        } else {
+            None
+        };
+    }
+    let mut presses_off = presses.clone();
+    presses_off.push(false);
+    let mut presses_on = presses.clone();
+    presses_on.push(true);
+    let mut lights_on = lights.clone();
+    lights_on ^= &machine.buttons[presses.len()];
+    let solution_off = solve_recurse(machine, lights, presses_off);
+    let solution_on = solve_recurse(machine, &lights_on, presses_on);
+    match (solution_off, solution_on) {
+        (Some(solution_off), Some(solution_on))
+            if solution_off.count_ones() <= solution_on.count_ones() =>
+        {
+            Some(solution_off)
+        }
+        (_, Some(solution_on)) => Some(solution_on),
+        (Some(solution_off), None) => Some(solution_off),
+        (None, None) => None,
+    }
+}
+
 #[aoc(day10, part1)]
 fn part1(input: &[Machine]) -> usize {
-    todo!()
+    input
+        .iter()
+        .map(|machine| configure(machine).count_ones())
+        .sum()
 }
 
 #[aoc(day10, part2)]
@@ -73,7 +113,11 @@ mod tests {
 
     #[test]
     fn part1_example() {
-        assert_eq!(part1(&parse(EXAMPLE)), 0);
+        let input = parse(EXAMPLE);
+        assert_eq!(configure(&input[0]), bitvec!(0, 0, 0, 0, 1, 1));
+        assert_eq!(configure(&input[1]), bitvec!(0, 0, 1, 1, 1));
+        assert_eq!(configure(&input[2]), bitvec!(0, 1, 1, 0));
+        assert_eq!(part1(&input), 7);
     }
 
     #[test]
